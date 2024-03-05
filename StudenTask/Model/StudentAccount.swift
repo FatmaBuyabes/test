3de665ed748ff37fbae5439fcd7090fcbc792a08
FatmaBuyabes <buyabesfatemah@gmail.com>
2024-03-05 17:15:10 +0300

    test

---
+++ StudenTask/Model/StudentAccount.swift
@@ -0,0 +1,8 @@
+//
+//  StudentAccount.swift
+//  StudenTask
+//
+//  Created by Fatma Buyabes on 03/03/2024.
+//
+
+import Foundation
---
+++ StudenTask/VC/studentTableViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  StudenTask
+//
+//  Created by Fatma Buyabes on 03/03/2024.
+//
+
+import UIKit
+import SnapKit
+
+class studentTableViewController: UITabBarController {
+
+    var 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
